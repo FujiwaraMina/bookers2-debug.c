b406a4917639d8f9ca73dccc39b9a56ceb617c08
EC2 Default User <ec2-user@ip-172-31-39-55.ap-northeast-1.compute.internal>
2022-10-06 07:54:40 +0000

    [Finish7c]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
-  has_one_attached :profile_image
   has_many :book_comments,dependent: :destroy
   has_many :favorites,dependent: :destroy
+  has_many :group_users
+  has_many :groups, through: :group_users
+  has_one_attached :profile_image
 
   # フォローをした、されたの関係
 has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
